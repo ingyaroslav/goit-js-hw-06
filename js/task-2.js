d68336e000b8fb1ed96e93e4fe863a43d4e61583
ingyaroslav <ingyaroslav@gmail.com>
2024-08-29 18:59:59 +0300

    +

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Storage {
+  #items = [];
+  constructor(items) {
+    items.forEach(element => this.#items.push(element));
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(el => el != itemToRemove);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
